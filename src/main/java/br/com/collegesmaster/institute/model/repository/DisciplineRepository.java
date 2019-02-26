8eb59f73a1bbec6228cfde05517cd83ee43ffbaf
diogo.muniz.s.brito <diogo.muniz.s.brito@CPX-C9V31ROHYT6.dir.svc.accenture.com>
2019-02-26 16:13:33 -0300

    Criado filtro para aplicar filtro de contains ao inves do de equals para o predicate de Discipline. Realizado mudança no repositoty de Discipline.

--- src/main/java/br/com/collegesmaster/institute/model/repository/DisciplineRepository.java
+++ src/main/java/br/com/collegesmaster/institute/model/repository/DisciplineRepository.java
@@ -4,15 +4,22 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
 import br.com.collegesmaster.institute.model.entity.Course;
 import br.com.collegesmaster.institute.model.entity.Discipline;
 import br.com.collegesmaster.institute.model.entity.impl.DisciplineImpl;
+import br.com.collegesmaster.institute.model.entity.impl.QDisciplineImpl;
 
 @Repository
 public interface DisciplineRepository extends JpaRepository<DisciplineImpl, Integer>,
-	QuerydslPredicateExecutor<DisciplineImpl> {
-
+	QuerydslPredicateExecutor<DisciplineImpl>, QuerydslBinderCustomizer<QDisciplineImpl> {
+	
+	@Override
+	default void customize(QuerydslBindings bindings, QDisciplineImpl root) {
+		bindings.bind(root.name).first((path, value) -> path.contains(value));
+	}
 	List<Discipline> findByCourse(final Course course);
 }
